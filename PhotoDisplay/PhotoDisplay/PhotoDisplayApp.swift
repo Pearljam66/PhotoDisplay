590eef2c0b98ce3caedccc5441ca1b449fac21c5
Sarah Clark <sarahireneclark@gmail.com>
2023-11-08 10:38:20 -0600

    Initial commit of project

---
+++ PhotoDisplay/PhotoDisplay/PhotoDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDisplayApp.swift
+//  PhotoDisplay
+//
+//  Created by Sarah Clark on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
