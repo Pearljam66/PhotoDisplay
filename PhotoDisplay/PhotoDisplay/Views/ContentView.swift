a32390e0a0ed03bd71b773b59c8b005cc44c3181
Sarah Clark <sarahireneclark@gmail.com>
2023-11-08 12:19:56 -0600

    Added images and scrollview

--- PhotoDisplay/PhotoDisplay/Views/ContentView.swift
+++ PhotoDisplay/PhotoDisplay/Views/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let images: [String] = [
+        "one", "two", "three", "four",
+        "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve",
+        "thirteen", "fourteen", "fifteen", "sixteen"
+    ]
+
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 300))
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                ForEach(images.indices) { i in
+                    Image(images[i])
+                        .resizable()
+                        .frame(width: 200, height: 300)
+                        .border(.black)
+                }
+            }
         }
-        .padding()
     }
 }
 
